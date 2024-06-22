499cdd9c820103d882f0e827de4d5f5126588e26
SantiagoDoval <danieldovals@gmail.com>
2024-06-22 10:35:10 -0500

    Home page ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import Reviews from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
+import Link from 'next/link'
 import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -25,15 +27,15 @@ export default function Home() {
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
                     High-quality, durable material
-                  </li>                  
+                  </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
                     5 years print guarantee
-                  </li>                  
+                  </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
                     Modern IPhone models supported
-                  </li>                  
+                  </li>
 
                 </div>
               </ul>
@@ -63,7 +65,7 @@ export default function Home() {
             <div className="relative md:max-w-xl">
               <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" alt="your image" />
               <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" alt="line" />
-              <Phone className="w-64" imgSrc="/testimonials/1.jpg"  />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
@@ -72,11 +74,11 @@ export default function Home() {
       <section className="bg-slate-100 py-24">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
-            <h2 className="order-1 mt-2 tracking-tight text-balance text-center !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our 
-              <span className="relative px-2">costumers <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />              
+            <h2 className="order-1 mt-2 tracking-tight text-balance text-center !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our
+              <span className="relative px-2">costumers <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />
               </span>say
-            </h2>            
-            <img src="/snake-2.png" className="w-24 order-0 lg:order-2"/>
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div>
           <div className="mx-auto grid mac-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
             {/* One user testimonial*/}
@@ -136,6 +138,56 @@ export default function Home() {
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-3 lg:px-8">
+            <div className="mx-12 max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-balance text-center !leading-tight font-bold text-5xl md:text-6xl text-gray-900">Upload you photo an get
+                <span className="relative px-2 bg-green-600 text-white">your own case</span>now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img src="/arrow.png" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0" alt="arrow" />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img src="/horse.jpg" className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full " alt="" />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              High-quiality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Screcth- and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              5 year print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link href='/configure/upload' className={buttonVariants({
+                size:'lg',
+                className:"mx-auto mt-8"
+              })}>Create you case now <ArrowRight className="h-4 w-4 ml-1.5"/></Link> 
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
